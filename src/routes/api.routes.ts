9559ddc2d1c01bd6f45de33d1ac675053b5d9946
Jimuel Palaca <jimuelpalaca@gmail.com>
2020-04-21 23:49:32 +0800

    Implement custom rest api response

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,9 +1,10 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request } from 'express';
+import CustomResponse from '../app/services/responses/CustomResponse';
 
 const apiRouter = Router();
 
-apiRouter.get('/', (req: Request, res: Response) => {
-    res.send('hello');
+apiRouter.get('/', (req: Request, res: CustomResponse) => {
+    res.withSuccess('Hello from Nodex');
 });
 
 export default apiRouter;
