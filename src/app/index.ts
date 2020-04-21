88a9b5240d24b9dfb9e7aafd7e6c7760d6d438ed
Jimuel Palaca <jimuelpalaca@gmail.com>
2020-04-21 11:05:05 +0800

    Initial setup for bodyParser middleware

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,8 @@
-import express from "express";
-import cors from "cors";
-import dotEnv from "dotenv";
-import corsConfig from "./config/cors";
+import cors from 'cors';
+import dotEnv from 'dotenv';
+import bodyParser from 'body-parser';
+import express from 'express';
+import corsConfig from './config/cors';
 
 // Initialize dotEnv configuration
 dotEnv.config();
@@ -9,10 +10,9 @@ dotEnv.config();
 const app = express();
 
 app.use(cors(corsConfig));
+app.use(bodyParser.json());
 
 app.listen(process.env.APP_PORT, () => {
-  // tslint:disable-next-line:no-console
-  console.log(
-    `${process.env.APP_NAME} server started at http://localhost:${process.env.APP_PORT}`
-  );
+    // tslint:disable-next-line:no-console
+    console.log(`${process.env.APP_NAME} server started at http://localhost:${process.env.APP_PORT}`);
 });
