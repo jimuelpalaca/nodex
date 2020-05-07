66a56b2f4c013bd1bde6d1054390f99af1f19ae8
Jimuel Palaca <jimuelpalaca@gmail.com>
2020-05-07 16:24:25 +0800

    Code improvement and refactoring for NodexResponse

--- src/app/http/response/NodexResponse.ts
+++ src/app/http/response/NodexResponse.ts
@@ -1,13 +1,15 @@
-import { Response } from 'express';
+import { Response as ExpressResponse } from 'express';
 import Transformer from '../mapper/Transformer';
 
-interface CustomResponse extends Response {
+interface NodexResponse extends ExpressResponse {
     withSuccess(message: string, metaData?: any[]): void;
     withItem(data: any, transformer?: Transformer<any>): void;
     withCollection(data: any[], transformer?: Transformer<any>): void;
     withException(exception: Error): void;
-    errorUnprocessableEntity(errors: any[], message?: string): void;
+    withErrors(errors: any[], message?: string): void;
     errorUnauthorized(message: string): void;
+    errorNotFound(message: string): void;
+    errorUnprocessableEntity(message: string): void;
 }
 
-export default CustomResponse;
+export default NodexResponse;
