4b38e57adda67665cb26ec866290638f371ec8bc
Jimuel Palaca <jimuelpalaca@gmail.com>
2020-05-07 14:30:26 +0800

    Install typeorm package

--- src/app/http/response/NodexResponse.ts
+++ src/app/http/response/NodexResponse.ts
@@ -4,6 +4,7 @@ import Transformer from '../mapper/Transformer';
 interface CustomResponse extends Response {
     withSuccess(message: string, metaData?: any[]): void;
     withItem(data: any, transformer?: Transformer<any>): void;
+    withCollection(data: any[], transformer?: Transformer<any>): void;
     withException(exception: Error): void;
     errorUnprocessableEntity(errors: any[], message?: string): void;
     errorUnauthorized(message: string): void;
